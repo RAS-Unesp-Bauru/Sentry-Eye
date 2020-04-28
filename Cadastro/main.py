a13441b4e0a9d7aad68eb08043118be3797ab1ea
Jokii <jokiinhofx@gmail.com>
2020-04-28 19:35:57 -0300

    alteração no arquivo main.py e add arquivo de facerec_test.py

--- Cadastro/main.py
+++ Cadastro/main.py
@@ -1,7 +1,10 @@
 from cadastro import Cadastro
+from facerec_test import FaceRecognition as fr
 
 p1 = Cadastro()
 p1.criar_pasta()
 p1.tirar_foto(0) # webcam = 0 / video = 1
 
 print(p1.dir)
+
+fr.FaceRecognition(p1.dir)
