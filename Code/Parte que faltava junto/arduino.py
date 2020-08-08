51990a5bf5440c3c56dc4ba22d899f3c0cce16c6
ViniPilan <vinicius.pilan@unesp.br>
2020-08-08 12:22:58 -0300

    Parte nono finalizada

---
+++ Code/Parte que faltava junto/arduino.py
@@ -0,0 +1,51 @@
+import serial 
+
+def createConnection(url): # Creates connection with the serial port of arduino.
+    print("Trying to connect with Arduino...")
+    try:
+        connection = serial.Serial(port=url, baudrate=9600, timeout=1) # Open serial port.
+        connection.flush()
+        print("Arduino connection was successful!")
+        return connection
+    except:
+        print("Arduino connection Error!")
+        return None
+
+def sendArduino(connection, direction1, rectangle1, direction2, rectangle2, jump_booster): # Sends the data for arduino.
+    jump1 = 0
+    jump2 = 0
+
+    if rectangle1 == 0:
+        jump1 = 1*jump_booster
+
+    if rectangle1 == 1:
+        jump1 = 2*jump_booster
+    
+    if rectangle1 == 2:
+        jump1 = 3*jump_booster
+    
+    if rectangle2 == 0:
+        jump2 = 1*jump_booster
+
+    if rectangle2 == 1:
+        jump2 = 2*jump_booster
+    
+    if rectangle2 == 2:
+        jump2 = 3*jump_booster
+    
+    data_string = "%s%s%s%s" % (direction1, jump1, direction2, jump2)
+    
+    print("Send to Arduino: ", data_string)
+
+    if connection is not None:    
+        connection.write(bytes(data_string, encoding='utf-8')) # Send a string to arduino.
+        connection.flush()                            
+
+def setServoInCenter(connection):
+    if connection is not None:    
+        connection.write(bytes('center', encoding='utf-8')) # Send a string to arduino.
+        connection.flush() 
+
+def closeConnection(connection): # Closes the connection with arduino.
+    if connection is not None:
+        connection.close() 
